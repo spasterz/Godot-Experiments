cba9d83c1459ca51204e3184fa8243c6df5b2f20
spasterz <steven.pasterz@gmail.com>
2025-09-15 17:22:25 -0700

    Added Main Scene

---
+++ dodge_the_creeps/Scenes/main.gd
@@ -0,0 +1,55 @@
+extends Node
+
+@export var mob_scene: PackedScene
+var score
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func game_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	
+func new_game():
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+	
+
+
+func _on_mob_timer_timeout():
+	var mob = mob_scene.instantiate()
+	
+	var mob_spawn_location = $MobPath/MobSpawnLocation
+	mob_spawn_location.progress_ratio = randf()
+	
+	mob.position = mob_spawn_location.position
+	
+	var direction = mob_spawn_location.rotation + PI / 2
+	
+	direction += randf_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+	
+	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	mob.linear_velocity = velocity.rotated(direction)
+	
+	add_child(mob)
+	
+	
+
+
+func _on_score_timer_timeout():
+	score += 1 # Replace with function body.
+
+
+func _on_start_timer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start() # Replace with function body.
--- dodge_the_creeps/Scenes/mob.gd
+++ dodge_the_creeps/Scenes/mob.gd
@@ -14,4 +14,4 @@ func _process(delta):
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
-	pass # Replace with function body.
+	queue_free() # Replace with function body.
