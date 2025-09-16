2ecd4aea64c967d07806e9c6d29fe3a46577cd45
spasterz <steven.pasterz@gmail.com>
2025-09-15 22:37:50 -0700

    Doge the Creeps: Finished Game Tutorial

--- dodge_the_creeps/Scenes/main.gd
+++ dodge_the_creeps/Scenes/main.gd
@@ -17,11 +17,18 @@ func _process(delta):
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 	
 func new_game():
+	get_tree().call_group("mobs", "queue_free")
 	score = 0
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready!")
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$Music.play()
 	
 
 
@@ -48,6 +55,7 @@ func _on_mob_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1 # Replace with function body.
+	$HUD.update_score(score)
 
 
 func _on_start_timer_timeout():
